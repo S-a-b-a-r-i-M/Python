f542bff24ad94aa98faddd5497d8ca670c5e9f06
S-a-b-a-r-i-M <sabarinithi2002@gmail.com>
2023-11-01 16:39:47 +0530

    minute changes

--- Practice.py
+++ Practice.py
@@ -8,3 +8,5 @@ if __name__ == '__main__':
 name,age,salary="sabari",21,30000   #name=sabari,age=21,salary=30000
 print(name,age,salary)
 print("my name is {} ,im {}years old my salary is ${}".format(name,age,salary))
+
+sabari
